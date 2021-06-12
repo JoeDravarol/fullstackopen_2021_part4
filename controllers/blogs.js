e5bfbd2b697959be487e11b2ec90b9efe56118a6
Joe <joedravarol@gmail.com>
2021-06-12 10:45:08 +0100

    Complete bloglist expansion exercise step7 feat: Adding new blogs is only possible if a valid token is sent with the HTTP POST request.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,3 +1,5 @@
+const config = require('../utils/config')
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
@@ -8,14 +10,26 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, config.SECRET)
+  
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({
+      error: 'token missing or invalid'
+    })
+  }
 
-  // array of one user as plain javascript object
-  const sampleUser = await User.aggregate(
-    [ { $sample: { size: 1 } } ]
-  )
-  const user = await User.findById(sampleUser[0]._id)
+  const user = await User.findById(decodedToken.id)
   
   const blog = new Blog({
     title: body.title,
